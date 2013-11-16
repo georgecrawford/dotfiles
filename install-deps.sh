b4878459a3826534b2851ba7e5d8b0dad0aecd2c
George Crawford <george.crawford@ft.com>
2013-11-16 09:53:28 +0000

    test whitespace

--- install-deps.sh
+++ install-deps.sh
@@ -5,6 +5,7 @@
 # the `push` command which copies the github compare URL to my clipboard is heaven
 sudo bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh)
 
+
 # https://rvm.io
 # rvm for the rubiess
 curl -L https://get.rvm.io | bash -s stable --ruby
