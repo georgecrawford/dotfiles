6527065ab167141318747ddc565e9ea82dbfbac8
George Crawford <george.crawford@ft.com>
2013-11-16 15:19:58 +0000

    Add 'z'. Tweak functions, adding useful git branch and git log functions.

--- .bash_profile
+++ .bash_profile
@@ -6,7 +6,7 @@ done
 unset file
 
 # init z   https://github.com/rupa/z
-#. ~/code/z/z.sh
+. ~/code/z/z.sh
 
 # init rvm
 #source ~/.rvm/scripts/rvm
@@ -20,7 +20,7 @@ unset file
 #export LANG="en_US"
 
 # Add tab completion for SSH hostnames based on ~/.ssh/config, ignoring wildcards
-#[ -e "$HOME/.ssh/config" ] && complete -o "default" -o "nospace" -W "$(grep "^Host" ~/.ssh/config | grep -v "[?*]" | cut -d " " -f2)" scp sftp ssh
+[ -e "$HOME/.ssh/config" ] && complete -o "default" -o "nospace" -W "$(grep "^Host" ~/.ssh/config | grep -v "[?*]" | cut -d " " -f2)" scp sftp ssh
 
 # Ensure all SSH keys are added
 ssh-add ~/.ssh/id_rsa >/dev/null 2>&1
@@ -28,7 +28,7 @@ ssh-add ~/.ssh/id_rsa >/dev/null 2>&1
 
 # Add tab completion for `defaults read|write NSGlobalDomain`
 # You could just use `-g` instead, but I like being explicit
-#complete -W "NSGlobalDomain" defaults
+complete -W "NSGlobalDomain" defaults
 
 
 # Initialize rbenv
--- install-deps.sh
+++ install-deps.sh
@@ -8,17 +8,17 @@ sudo bash < <( curl https://raw.github.com/jamiew/git-friendly/master/install.sh
 
 # https://rvm.io
 # rvm for the rubiess
-curl -L https://get.rvm.io | bash -s stable --ruby
+# curl -L https://get.rvm.io | bash -s stable --ruby
 
 # https://github.com/isaacs/nave
 # needs npm, obviously.
 # TODO: I think i'd rather curl down the nave.sh, symlink it into /bin and use that for initial node install.
-npm install -g nave
+# npm install -g nave
 
 
 # homebrew!
 # you need the code CLI tools YOU FOOL.
-ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
+# ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
 
 
 
@@ -31,17 +31,17 @@ chmod +x ~/code/z/z.sh
 # add this to the bash_profile file if it aint there.
 #   . ~/code/z/z.sh
 
-cd ~/code
-git clone git://github.com/dronir/SpotifyControl.git
+# cd ~/code
+# git clone git://github.com/dronir/SpotifyControl.git
 
 # add DropBox uploader
-git clone git://github.com/andreafabrizi/Dropbox-Uploader.git
+# git clone git://github.com/andreafabrizi/Dropbox-Uploader.git
 
 
 # my magic photobooth symlink -> dropbox. I love it.
 # first move Photo Booth folder out of Pictures and into dropbox/public
 # then:
-ln -s ~/Dropbox/Public/Photo\ Booth/ ~/Pictures/Photo\ Booth
+# ln -s ~/Dropbox/Public/Photo\ Booth/ ~/Pictures/Photo\ Booth
 # now you can record photobooth videos quickly and they upload to dropbox DURING RECORDING
 # then you grab public URL and send off your video message in a heartbeat.
 
